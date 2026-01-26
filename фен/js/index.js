61d601a500851bb6d61c5910e3ff08a7817ca583
gynch0 <106382132+gynch0@users.noreply.github.com>
2026-01-26 20:16:40 +0400

    fen

--- фен/js/index.js
+++ фен/js/index.js
@@ -1,5 +1,17 @@
+// $(document).ready(function () {
+// 	$(".header__menu ").on("click", function () {
+// 		$(".header__menu header__menu-list").toggleClass("header__menu");
+// 	});
+// });
+
 $(document).ready(function () {
-	$(".header__menu ").on("click", function () {
-		$("header__menu header__menu-list").toggleClass("header__menu");
+	$(".header__burger").on("click", function () {
+		// переключаем класс на кнопке для иконок
+		$(this).toggleClass("burger--open");
+		$("#header__menu").toggleClass("header__menu--open");
+
+		// обновляем aria-expanded
+		const expanded = $(this).hasClass("burger--open");
+		$(this).attr("aria-expanded", expanded);
 	});
 });
